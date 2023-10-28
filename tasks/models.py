02853796b96a29910ab50c5b58c896bf3162fc79
Vlad Larin <lordgadyka71@gmail.com>
2023-10-28 16:36:10 +0300

    Initialize project, add .gitignore, add Tag & Task models

---
+++ tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+    deadline = models.DateField(blank=True)
+    completed = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
