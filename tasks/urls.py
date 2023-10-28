367993f0e62584b2749f9a7dbef7662da27782be
Vlad Larin <lordgadyka71@gmail.com>
2023-10-28 18:15:16 +0300

    Add task-create & task-update views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from tasks.views import IndexView
+from tasks.views import TaskListView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("", TaskListView.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"
+    ),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,24 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from tasks.models import Task, Tag
 
 
-class IndexView(generic.ListView):
+class TaskListView(generic.ListView):
     model = Task
     template_name = "tasks/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
 
 
 class TagView(generic.ListView):
